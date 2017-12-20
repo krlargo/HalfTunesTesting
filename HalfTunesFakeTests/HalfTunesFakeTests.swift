ea3a6d9442ccd315fb08252927fd1ae75a403808
krlargo <krlargo@ucdavis.edu>
2017-12-19 18:58:53 -0800

    Add setup()

--- HalfTunesFakeTests/HalfTunesFakeTests.swift
+++ HalfTunesFakeTests/HalfTunesFakeTests.swift
@@ -16,6 +16,18 @@ class HalfTunesFakeTests: XCTestCase {
     override func setUp() {
         super.setUp()
         controllerUnderTest = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! SearchViewController!;
+        
+        
+        let testBundle = Bundle(for: type(of: self))
+        let path = testBundle.path(forResource: "test", ofType: "json")
+        let data = try? Data(contentsOf: URL(fileURLWithPath: path!), options: .alwaysMapped)
+        
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        let urlResponse = HTTPURLResponse(url: url!, statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        let sessionMock = URLSessionMock(data: data, response: urlResponse, error: nil)
+        
+        controllerUnderTest.defaultSession = sessionMock;
     }
     
     override func tearDown() {
@@ -23,16 +35,30 @@ class HalfTunesFakeTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // Fake URLSession with DHURLSession protocol and stubs
+    func test_UpdateSearchResults_ParsesData() {
+        // given
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        XCTAssertEqual(controllerUnderTest?.searchResults.count, 0, "searchResults should be empty before the data task runs")
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        let dataTask = controllerUnderTest?.defaultSession.dataTask(with: url!) {
+            data, response, error in
+            // if HTTP request is successful, call updateSearchResults(_:) which parses the response data into Tracks
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    promise.fulfill()
+                    self.controllerUnderTest?.updateSearchResults(data)
+                }
+            }
         }
+        dataTask?.resume()
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // then
+        XCTAssertEqual(controllerUnderTest?.searchResults.count, 3, "Didn't parse 3 items from fake response")
     }
-    
 }
